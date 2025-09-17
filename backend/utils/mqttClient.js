4c22e45d40c882476311ce32eaf8d5e4423b5bbb
lanuxos <lanuxos@gmail.com>
2025-09-17 15:28:51 +0700

    mqtt hivemq acc

--- backend/utils/mqttClient.js
+++ backend/utils/mqttClient.js
@@ -4,7 +4,10 @@ const mqtt = require('mqtt');
 // connect to broker (can be public broker.emqx.io or your own)
 // const client = mqtt.connect('mqtt://broker.emqx.io:1883');
 // above endpoint work fine on localhost
-const client = mqtt.connect('wss://broker.hivemq.com:443/mqtt');
+const client = mqtt.connect('48c086e2145d4b3eb554c49bcd039382.s1.eu.hivemq.cloud', {
+    username: 'mernbook',
+    password: 'T00r@123'
+});
 
 client.on('connect', () => {
     console.log('✅ MQTT connected from backend');
