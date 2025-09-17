0b41499a4ba7694200386a8dc61c462a5f9f9eaa
lanuxos <lanuxos@gmail.com>
2025-09-17 15:03:33 +0700

    mqtt protocal+path update

--- backend/utils/mqttClient.js
+++ backend/utils/mqttClient.js
@@ -4,7 +4,10 @@ const mqtt = require('mqtt');
 // connect to broker (can be public broker.emqx.io or your own)
 // const client = mqtt.connect('mqtt://broker.emqx.io:1883');
 // above endpoint work fine on localhost
-const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt');
+const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt', {
+    protocol: 'wss',
+    path: '/mqtt',
+});
 
 client.on('connect', () => {
     console.log('✅ MQTT connected from backend');
